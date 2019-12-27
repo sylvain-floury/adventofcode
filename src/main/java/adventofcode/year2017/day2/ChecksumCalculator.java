8202670ac3b3154659cae184a8925d27627bec7f
Sylvain Floury <sylvain@floury.name>
2019-12-27 17:28:33 +0100

    checksum implementation for 2017 day 2 puzzle

---
+++ src/main/java/adventofcode/year2017/day2/ChecksumCalculator.java
@@ -0,0 +1,33 @@
+package adventofcode.year2017.day2;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class ChecksumCalculator {
+    public static int checksum(String spreadsheet) {
+        return Stream.of(spreadsheet.split("\\n"))
+                .mapToInt(ChecksumCalculator::difference)
+                .sum();
+    }
+
+    public static int difference(String row) {
+        return max(row) - min(row);
+    }
+
+    public static int min(String row) {
+        return splitIntoStream(row)
+                .min()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid row"));
+    }
+
+    public static int max(String row) {
+        return splitIntoStream(row)
+                .max()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid row"));
+    }
+
+    private static IntStream splitIntoStream(String row) {
+        return Stream.of(row.split("\\s+"))
+                .mapToInt(Integer::parseInt);
+    }
+}
